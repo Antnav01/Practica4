21e335ac38a10ec2b1cfd4e8d36ca13b3f006178
Amparo Rubio Bellón <37449976+amprubio@users.noreply.github.com>
2018-04-17 20:07:00 +0200

    Reorganizacion de la practica y metodos en lista InsertarInicio y BuscaNodo

---
+++ Practica4/Practica4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Lista;
+namespace Practica4
+{
+	class MainClass
+	{
+		public static void Main ()
+		{
+			
+		}
+	}
+}
---
+++ Practica4/Practica4/WallE.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Lista;
+namespace WallE
+{
+		// posibles direcciones
+		public enum Direction {North,South,East,West};
+		class Map{
+			// items basura
+			struct Item{
+				public string name, description;
+			}
+			// lugares del mapa
+			struct Place {
+				public string name, description;
+				public bool spaceShip;
+				public int [] connections; // vector de 4 componentes
+				// con el lugar al norte, sur, este y oeste
+				// -1 si no hay conexion
+				public Lista itemsInPlace; // liste de enteros, indices al vector de items
+			}
+			Place [] places; // vector de lugares del mapa
+			Item [] items; // vector de items del juego
+		int nPlaces, nItems;
+		public Map(){
+			places= new Place[nPlaces];
+			items= new Item[nItems];
+
+		}
+	}
+}
